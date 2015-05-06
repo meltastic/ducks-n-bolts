a3727f33a0ce657da301bca6beaa0e06bebbcf41
meltastic <mhargis@csusm.edu>
2015-05-06 08:50:15 -0700

    changed port

--- app.js
+++ app.js
@@ -96,6 +96,10 @@ app.get('/search/:word?', function (req, res) {
     res.render('myword', resObj);
 });
 
-http.createServer(app).listen(app.get('port'), function () {
+//http.createServer(app).listen(app.get('port'), function () {
+//    console.log('Express server listening on port ' + app.get('port'));
+//});
+
+http.createServer(app).listen(process.env.PORT, function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
